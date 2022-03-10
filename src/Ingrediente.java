565220982436306e414bc499bf32faf1ada56d52
liz15243 <lizethespi11@gmail.com>
2022-03-10 11:32:57 -0600

    Initial commit

---
+++ src/Ingrediente.java
@@ -0,0 +1,12 @@
+public class Ingrediente {
+    private String nombre;
+    private boolean salado;
+    private double peso;
+
+
+    public Ingrediente(String nombre, boolean salado, double peso) {
+        this.nombre = nombre;
+        this.salado = salado;
+        this.peso = peso;
+    }
+}

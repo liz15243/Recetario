8a59730f6bd96c976cdc36bc4cfcb16f022db7b3
liz15243 <lizethespi11@gmail.com>
2022-03-10 11:38:00 -0600

    Clase ingrediente

--- src/Ingrediente.java
+++ src/Ingrediente.java
@@ -3,10 +3,35 @@ public class Ingrediente {
     private boolean salado;
     private double peso;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSalado() {
+        return salado;
+    }
+
+    public void setSalado(boolean salado) {
+        this.salado = salado;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
 
     public Ingrediente(String nombre, boolean salado, double peso) {
         this.nombre = nombre;
         this.salado = salado;
         this.peso = peso;
+        System.out.println("Ingrediente creado");
     }
 }
